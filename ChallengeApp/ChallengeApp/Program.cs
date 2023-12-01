995de8f82e2a01b11380899be20b48244b8884bd
calemb79 <calemb79@gmail.com>
2023-12-01 23:16:48 +0100

    zadanie

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var sex = "Kobieta";
+var sexm = "Mężczyzna";
+var age = 30;
+//var age2 = 30;
+//var age3 = 33;
+
+if (sex == "Kobieta" && age - 1 < 30)
+    Console.WriteLine("Kobieta poniżej 30 lat");
+if (name == "Ewa" && age + 3 == 33)
+    Console.WriteLine("Ewa, lat 33");
+if (age - 13 < 18 && sexm != "Kobieta")
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+if (age - 13 > 18 && sexm == "Mężczyzna")
+    Console.WriteLine("Pełnoletni Mężczyzna");
+else Console.WriteLine("*KONIEC*");
