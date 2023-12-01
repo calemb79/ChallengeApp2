1818ff1daae82646fe59763728b01627b603a01b
calemb79 <152223475+calemb79@users.noreply.github.com>
2023-12-02 00:07:29 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//var myAge = 413213131324;
+//int myAge2 = 26;
+//Console.WriteLine(myAge+myAge2);
+
+//ulong myAge3 = ulong.MaxValue;
+
+//float myAge3 = 3.55464465f;
+//Console.WriteLine(myAge3);
+
+
+//bool prawda = true;
+//prawda = false;
+//var wynik = 5 < 6;
+//Console.WriteLine(wynik);
+
+var nr1 = 10;
+var nr2 = 10.5;
+
+if (nr1 > nr2)
+{
+    Console.WriteLine("ok");
+}
+else
+{
+    Console.WriteLine("no");
+    }
+
